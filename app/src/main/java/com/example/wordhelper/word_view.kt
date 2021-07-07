0dee553e7023dbc7afdb003cfbbfb24036458f13
yudonggeun <ydong98@gmail.com>
2021-07-07 21:38:44 +0900

    first init

---
+++ app/src/main/java/com/example/wordhelper/word_view.kt
@@ -0,0 +1,11 @@
+package com.example.wordhelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class word_view : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_word_view)
+    }
+}
