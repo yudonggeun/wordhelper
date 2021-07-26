96cd9ffe220377bb28b2c40d29b0a8e00dc5958e
yudonggeun <ydong98@gmail.com>
2021-07-26 15:26:57 +0900

    gradle 파일 추가 및 단어 테스트 뷰 플리퍼 기능 추가

--- app/src/main/java/com/example/wordhelper/word_test.kt
+++ app/src/main/java/com/example/wordhelper/word_test.kt
@@ -3,23 +3,27 @@ package com.example.wordhelper
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_word_test.*
 
 class word_test : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word_test)
+        var csvName : ArrayList<String> = ArrayList()
+
         btnRandomtest.setOnClickListener {
             var intent : Intent = Intent(applicationContext, word_test_activity::class.java)
+            intent.putStringArrayListExtra("files", csvName)
             startActivity(intent);
         }
         btnWeektest.setOnClickListener {
             var intent : Intent = Intent(applicationContext, word_test_activity::class.java)
+            intent.putStringArrayListExtra("files", csvName)
             startActivity(intent);
         }
         btnMultitest.setOnClickListener {
             var intent : Intent = Intent(applicationContext, word_test_activity::class.java)
+            intent.putStringArrayListExtra("files", csvName)
             startActivity(intent);
         }
     }
--- app/src/main/java/com/example/wordhelper/word_test_activity.kt
+++ app/src/main/java/com/example/wordhelper/word_test_activity.kt
@@ -1,11 +1,124 @@
 package com.example.wordhelper
-
+//apache poi api를 사용하여 xls 파일을 읽는 기능
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_word_test_activity.*
+import org.apache.poi.hssf.usermodel.HSSFCell
+import org.apache.poi.hssf.usermodel.HSSFRow
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.openxml4j.opc.OPCPackage
+import org.apache.poi.poifs.filesystem.POIFSFileSystem
+import org.apache.poi.ss.usermodel.*
+import org.apache.poi.ss.util.CellReference
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import java.io.*
+import java.lang.Exception
+import java.nio.file.Paths
 
 class word_test_activity : AppCompatActivity() {
+    lateinit var path : String
+    lateinit var wordList : ArrayList<String>
+    lateinit var detailList : ArrayList<ArrayList<String>>
+    var size : Int = 0
+    var index : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word_test_activity)
+        wordList = ArrayList<String>()
+        detailList = ArrayList<ArrayList<String>>()
+
+        path = filesDir.toString()+"/"
+        fileNameView.text = "voca_bible_day1.xlsx"
+        var target : String ="voca_bible_day1.xlsx"
+        //readExcelFileFromAssets(path + target)
+        setTestList()
+        initClickListener()
+        addViewFlipper()
+        fileNameView
+
+        progressView
+
+
+        progressView.text = (index+1).toString() + "/" + size
+    }
+    data class SearchData(val word : String, val detail : String)
+    private fun initClickListener(){
+        btnShowdetail.setOnClickListener {
+            //dialog
+        }
+        wordView.setOnClickListener {
+            wordView.showNext()
+            index = (index+1)%size
+            progressView.text = (index+1).toString() + "/" + size
+        }
+    }
+    private fun addViewFlipper(){
+        for(word in wordList){
+            val Textview = TextView(this)
+            val layoutParams = FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.gravity = Gravity.CENTER
+            Textview.layoutParams = layoutParams
+            Textview.gravity = Gravity.CENTER
+            Textview.text = word
+            Textview.textSize = 70f
+            wordView.addView(Textview)
+            size++
+        }
+    }
+    private fun setTestList(){
+        var test = arrayOf("apple", "banana", "graph", "mango", "orange")
+        var test_detail = arrayOf(
+            arrayOf("사과", "뉴턴의 사과", "잘못된 뜻"),
+            arrayOf("바나나"),
+            arrayOf("포도"),
+            arrayOf("망고"),
+            arrayOf("오랜지")
+        )
+        for(a in test) wordList.add(a)
+        for(a in test_detail){
+            var index = test_detail.indexOf(a);
+            detailList.add(ArrayList<String>())
+            for(b in a){
+                detailList.get(index).add(b)
+            }
+        }
+    }
+    private fun readExcelFileFromAssets(path : String) {
+        val pkg = OPCPackage.open(File(path))
+        val wb : Workbook = XSSFWorkbook(pkg)
+        var sheet : Sheet = wb.getSheetAt(0)
+        for(row in sheet){
+            detailList.add(ArrayList<String>())
+            for(cell in row){
+                var cellRef : CellReference = CellReference(row.rowNum, cell.columnIndex)
+
+                when(cell.columnIndex){
+                    0 ->{//word
+                        if(cell.cellType == CellType.STRING){
+                            wordList.add(cell.stringCellValue)
+                        }
+                    }
+                    1->{//detail
+                        var detail  = detailList.get(row.rowNum)
+                        if(cell.cellType == CellType.STRING){
+                            detail.add(cell.stringCellValue)
+                        }
+                    }
+                }
+            }
+        }
+        pkg.close()
     }
 }
